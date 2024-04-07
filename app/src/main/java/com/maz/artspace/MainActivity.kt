8900457c76f9b2b08edb4f3a106412081bee2670
Dylan Maziarek <21125921+m-a-z-z-z@users.noreply.github.com>
2024-04-07 15:52:53 +0100

    Bones of project done

--- app/src/main/java/com/maz/artspace/MainActivity.kt
+++ app/src/main/java/com/maz/artspace/MainActivity.kt
@@ -3,13 +3,32 @@ package com.maz.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.maz.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +48,71 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ArtSpaceLayout() {
+    var currentImage by remember { mutableStateOf(1) }
+    val imageResourceID = when (currentImage) {
+        1 -> R.drawable.hand
+        2 -> R.drawable.fred
+        3 -> R.drawable.tupac
+        4 -> R.drawable.shawty
+        5 -> R.drawable.mc_ride
+        else -> R.drawable.me_basically
+    }
+    val imageDescriptionID = when (currentImage) {
+        1 -> R.string.hand
+        2 -> R.string.fred
+        3 -> R.string.tupac
+        4 -> R.string.hand
+        5 -> R.string.mc_ride
+        else -> R.string.me_basically
+    }
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Art Space", fontWeight = FontWeight.Bold)
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                )
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.background)
+        ){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ArtPieceAndButtons(imageResource = imageResourceID, imageDescription = imageDescriptionID)
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtPieceAndButtons(imageResource: Int, imageDescription: Int, modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(painter = painterResource(imageResource), contentDescription = null,
+                modifier = Modifier
+                    .padding(24.dp)
+            )
+        }
+    }
 }
 
 @Preview
